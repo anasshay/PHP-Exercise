1015bc73fcdf2399d2b14b51ed9db80a1b250295
anasshay <anasshaydar5@gmail.com>
2022-02-24 23:08:19 +0200

    step5

---
+++ Basics-02/step5.php
@@ -0,0 +1,39 @@
+<?php 
+function s($n){
+    switch ($n) {
+  case "zero":
+    return 0;
+    break;
+  case "one":
+    return 1;
+    break;
+  case "two":
+    return 2;
+    break;
+  case "three":
+    return 3;
+    break;
+  case "four":
+    return 4;
+    break;
+  case "five":
+    return 5;
+    break;
+  case "six":
+    return 6;
+    break;
+  case "seven":
+    return 7;
+    break;
+  case "eight":
+    return 8;
+    break;
+  case "nine":
+    return 9;
+    break;
+};
+}
+
+echo s('zero')
+
+?>
