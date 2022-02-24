dc918efae04132c55adad55e3b356003bd289ba1
anasshay <anasshaydar5@gmail.com>
2022-02-24 12:22:19 +0200

    step3

---
+++ step3.php
@@ -0,0 +1,10 @@
+<?php
+$name = "Anass";
+$email = "anasshaydr5@gmail.com";
+echo "My name is " . $name . "and my eamil is " . $email . "\n";
+
+$height = 173;
+$width = 50;
+$area = $height * $width;
+
+echo $area;
---
+++ step_2.php
@@ -0,0 +1,4 @@
+"Hello world, Help me I am using PHP"
+<?php
+print "Search on how to enable PHP Errors";
+?>
